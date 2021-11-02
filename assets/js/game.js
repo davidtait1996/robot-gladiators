41c6268a12b34136bc05e4549216cafd0d27c143
Smoom <david.tait.96@gmail.com>
2021-11-01 22:40:48 -0600

    multiple enemies in array

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,14 @@ var playerMoney = 10;
 
 console.log(playerName, playerHealth, playerAttack);
 
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators");
 
     var promptFight = window.prompt("Would you like to attack? FIGHT for fight, SKIP to skip");
@@ -19,9 +22,6 @@ var fight = function() {
     if (promptFight === "fight" || promptFight === "FIGHT") {
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
         enemyHealth = enemyHealth - playerAttack;
-
-        // Log a resulting message to the console so we know that it worked.
-        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
 
         if(enemyHealth <= 0) {
             window.alert(enemyName + " has died!");
@@ -32,9 +32,6 @@ var fight = function() {
 
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
         playerHealth = playerHealth - enemyAttack;
-
-        // Log a resulting message to the console so we know that it worked.
-        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
         if (playerHealth <= 0) {
             window.alert(playerName + " has died!");
@@ -63,4 +60,6 @@ var fight = function() {
 
 }
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
