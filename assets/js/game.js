5863345e05ea49e1fc4e1299f714e59a114e416f
Smoom <david.tait.96@gmail.com>
2021-11-05 16:39:15 -0600

    lower case fight/skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,20 +56,31 @@ var enemyInfo = [
     }
 ];
 
+var fightOrSkip = function() {
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+  promptFight = promptFight.toLowerCase();
+
+  if(promptFight === "" || promptFight === null) {
+    window.alert("u dum bitch give a valid answer");
+    return fightOrSkip();
+  }
+
+  if(promptFight === "skip") {
+    var confirmSkip = window.confirm("Are you sure you want to quit?");
+    if(confirmSkip) {
+      window.alert(playerInfo.name + " has decided ot skip this fight. bye bye money")
+      playerInfo.money = playerInfo.money - 10;
+      return true;
+    }
+
+  }
+}
+
 var fight = function (enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
-    var promptFight = window.prompt("Would you like to attack? FIGHT for fight, SKIP to skip");
 
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-      if (confirmSkip) {
-        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-        playerInfo.money = playerInfo.money - 10;
-        break;
-      } else {
-        fight();
-      }
+    if(fightOrSkip()){
+      break;
     }
 
     //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
